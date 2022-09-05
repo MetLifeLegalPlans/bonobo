0a059e2824c3636af2cc57bf3fbf5b5d5f2fe6bd
Ian MacLeod <ian.a.macleod@gmail.com>
2022-09-05 16:33:01 -0700

    Bump python-slugify version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     install_requires=[
         'cached-property ~= 1.4', 'fs ~= 2.0', 'graphviz >= 0.8, < 0.9',
         'jinja2 ~= 2.9', 'mondrian ~= 0.8', 'packaging ~= 19.0',
-        'psutil ~= 5.4', 'python-slugify ~= 1.2.0', 'requests ~= 2.0',
+        'psutil ~= 5.4', 'python-slugify ~= 5.0', 'requests ~= 2.0',
         'stevedore ~= 1.27', 'whistle ~= 1.0'
     ],
     extras_require={
